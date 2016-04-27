da2a2c93b9543f5be8039c43118834529314d160
effner <b.effner@tu-braunschweig.de>
2016-04-27 11:10:28 +0200

    Konstruktoren erstellt

--- Pokemon.java
+++ Pokemon.java
@@ -1,17 +1,17 @@
 
 public class Pokemon {
 	
-	int Nr;
-	String Name;
-	String Type1;
-	String Type2;
-	int Total;
-	int HP;
-	int Attack;
-	int Defence;
-	int SpAtk;
-	int SpDef;
-	int Speed;
+	private int Nr;
+	private String Name;
+	private String Type1;
+	private String Type2;
+	private int Total;
+	private int HP;
+	private int Attack;
+	private int Defence;
+	private int SpAtk;
+	private int SpDef;
+	private int Speed;
 	
 	Pokemon(int Nr, 
 			String Name, 
@@ -37,4 +37,37 @@ public class Pokemon {
 		this.Speed = Speed;
 	}
 	
+	public int getNr(){
+		return this.Nr;
+	}
+	public String getName(){
+		return this.Name;
+	}
+	public String getType1(){
+		return this.Type1;
+	}
+	public String getType2(){
+		return this.Type2;
+	}
+	public int getTotal(){
+		return this.Total;
+	}
+	public int getHP(){
+		return this.HP;
+	}
+	public int getAttack(){
+		return this.Attack;
+	}
+	public int getDefence(){
+		return this.Defence;
+	}
+	public int getSpAtk(){
+		return this.SpAtk;
+	}
+	public int getSpDef(){
+		return this.SpDef;
+	}
+	public int getSpeed(){
+		return this.Speed;
+	}
 }
