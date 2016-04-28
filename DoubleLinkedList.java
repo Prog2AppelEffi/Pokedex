3014db71dd78dea09f5539a72732827dc8a61e16
Martin Appelmann <m.appelmann@tu-bs.de>
2016-04-28 14:38:44 +0200

    Objekte(Pokemon) werden erstellt
    Für jede beschriebene Zeile wird ein Pokemon erstellt und die Attribute
    werden in den Pokemon Konstruktor zerlegt und erstellt.

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,4 +1,39 @@
 
 public class DoubleLinkedList implements List {
 
-}
+	private Pokemon obj;
+	private DoubleLinkedList nextElement;
+	private DoubleLinkedList preElement;
+
+	public DoubleLinkedList(Pokemon obj){
+		this.obj = obj;
+		nextElement = null;
+	}
+
+
+	
+
+
+		
+
+	public boolean isEmpty(){
+		return true;
+	}
+
+	public int length(){
+		return 2;
+	}
+
+	public Pokemon firstPokemon(){
+		Pokemon test = new Pokemon("460,Abomasnow,Grass,Ice,494,90,92,75,92,85,60");
+		return test;
+	}
+
+	public void insert(Pokemon p){
+		DoubleLinkedList list = new DoubleLinkedList(p);
+	}
+
+	public void	delete(Pokemon p){
+		
+	}
+}	
